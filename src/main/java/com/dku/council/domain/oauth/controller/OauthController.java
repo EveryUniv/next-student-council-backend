767d903ea82fd3e86c07f753845c0bb19e9bcce6
juy4556 <wnstjrtmdgus@gmail.com>
2024-05-17 19:09:06 +0900

    feature: 약관 확인 핸들러 /oauth/term 추가

--- src/main/java/com/dku/council/domain/oauth/controller/OauthController.java
+++ src/main/java/com/dku/council/domain/oauth/controller/OauthController.java
@@ -2,6 +2,7 @@ package com.dku.council.domain.oauth.controller;
 
 import com.dku.council.domain.oauth.model.dto.request.OauthLoginRequest;
 import com.dku.council.domain.oauth.model.dto.request.OauthRequest;
+import com.dku.council.domain.oauth.model.dto.request.OauthTermsRequest;
 import com.dku.council.domain.oauth.model.dto.request.TokenExchangeRequest;
 import com.dku.council.domain.oauth.model.dto.response.TokenExchangeResponse;
 import com.dku.council.domain.oauth.service.OauthService;
@@ -50,4 +51,9 @@ public class OauthController {
         return ResponseEntity.ok(response);
     }
 
+    @PostMapping("/terms")
+    public RedirectView verifyTerms(@RequestBody OauthTermsRequest request) {
+        String uri = oauthService.verifyTerms(request.getStudentId(), request.toOauthInfo());
+        return new RedirectView(uri);
+    }
 }
